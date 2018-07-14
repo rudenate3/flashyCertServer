a75dd58c6a7bcd4a3c8722ecd96bdf98563e345e
rudenate3 <nathan.odin@gmail.com>
2018-07-14 12:10:30 -0400

    Add authenticated routes
    -(feature) Add auth guard to authenticated routes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,23 @@
 const express = require('express'),
+  passport = require('passport'),
   router = express.Router()
 
 const authController = require('../controllers/auth.controller'),
   examsController = require('../controllers/exam.controller')
 
+const authenticated = () => {
+  return passport.authenticate('jwt', { session: false })
+}
+
 module.exports = router
   // Auth Routes
   .post('/register', authController.register)
   .post('/login', authController.login)
-  .patch('/change-password', authController.changePassword)
-  .delete('/logout', authController.logout)
+  .patch('/change-password', authenticated(), authController.changePassword)
+  .delete('/logout', authenticated(), authController.logout)
   // Exam Routes
-  .get('/exams/', examsController.index)
-  .get('/exams/:id', examsController.show)
-  .post('/exams/', examsController.create)
-  .put('/exams/:id', examsController.update)
-  .delete('/exams/:id', examsController.destroy)
+  .get('/exams/', authenticated(), examsController.index)
+  .get('/exams/:id', authenticated(), examsController.show)
+  .post('/exams/', authenticated(), examsController.create)
+  .put('/exams/:id', authenticated(), examsController.update)
+  .delete('/exams/:id', authenticated(), examsController.destroy)
