1856790f46ec927d24b2fb3ebf81fa4974930087
rudenate3 <nathan.odin@gmail.com>
2018-07-13 23:48:39 -0400

    Add JWT and sign token
    -(feature) Sign token after user validated in AuthController
    -(deps) Install jsonwebtokens

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+
 const User = require('../models/user.model')
 
 exports.register = (req, res, next) => {
@@ -19,10 +21,23 @@ exports.login = (req, res, next) => {
     if (err) {
       res.send(err)
     } else {
-      res.json({
-        success: true,
-        message: 'User Validated'
-      })
+      const payload = {
+        id: user.id,
+        username: user.username
+      }
+
+      jwt.sign(
+        payload,
+        process.env.jwtKey,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) res.send(err)
+          res.json({
+            success: true,
+            token: `Bearer ${token}`
+          })
+        }
+      )
     }
   })
 }
